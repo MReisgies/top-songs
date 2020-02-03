269b8ceb761818c11c2b4c88908e3c16f3940737
i02401246 <markus.reisgies@endress.com>
2020-02-03 16:05:50 +0100

    ./

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var app = express();
 app.use(express.static('public'));
  
 //make way for some custom css, js and images
-app.use('/css', express.static('/public/css'));
-app.use('/js', express.static('/public/js'));
-app.use('/images', express.static('/public/pictures'));
+app.use('/css', express.static('./public/css'));
+app.use('/js', express.static('./public/js'));
+app.use('/images', express.static('./public/pictures'));
  
 var server = app.listen(8081, function(){
     var port = server.address().port;
