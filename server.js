23fc50bf70b3e79353a06b26c4d2198ff499f846
markit0 <training.info.solutions@gmail.com>
2020-02-09 23:29:42 +0100

    folder structure simplified

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var app = express();
 app.use(express.static('public'));
  
 //make way for some custom css, js and images
-app.use('css', express.static('public/css'));
-app.use('js', express.static('public/js'));
-app.use('images', express.static('public/images'));
+app.use('/static', express.static('public'));
+/* app.use('/static', express.static('public/js'));
+app.use('/static', express.static('public/images')); */
  
 var server = app.listen(8081, function(){
     var port = server.address().port;
